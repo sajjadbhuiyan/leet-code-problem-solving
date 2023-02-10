0dbc9a4c2cd7a656e9be7c42e4fea26a939cfae3
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2023-02-09 18:26:06 -0800

    remove duplicate element from array and remove element porblem solved

---
+++ Array/removeElement.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+  return nums.length;
+};
+
+const nums = [0, 1, 2, 2, 3, 0, 4, 2, 2, 2, 2];
+const val = 2;
+
+removeElement(nums, val);
